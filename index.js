14d4d5a9fef2f9f4619312872f3edd39d35ccacf
Evan Chernicky <echernicky@gmail.com>
2022-01-08 10:57:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const removeMain = document.getElementsByTagName("main")[0];
+
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+const grabBody = document.getElementsByTagName("body")[0];
+
+grabBody.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
